1072f9a4efb1a91a7e9f6f42b92e8f57eddde0e5
Ethan <112027599+ethantl-1511@users.noreply.github.com>
2023-01-30 17:24:22 -0600

    Update Program.cs

--- 1. Two Sum/1. Two Sum/Program.cs
+++ 1. Two Sum/1. Two Sum/Program.cs
@@ -14,20 +14,22 @@ namespace _1._Two_Sum
 
             int[] TwoSumArray = new int[2]; // blank array to add numbers into for result
 
+            // use two iterations, first to hold value of first iteration to check value of section iteration
             for (int i = 0; i < nums.Length; i++)
             {
-                int iterationNum = nums[i];
+                int iterationNum = nums[i]; // set value of array position, index i to variable
                 for (int j = 0; j < nums.Length; j++)
                 {
-                    int jiterationNum = nums[j];
-                    if (i == j) // if the iteration of i is the same iteration as j, skip it 
+                    int jiterationNum = nums[j]; // set value of array position, index j to variable
+                    if (i == j) // if the iteration of i is the same iteration as j, 
+                                // skip it because we don't want [0,0], [1,1], etc.
                     {
                         continue;
                     }
-                    if (iterationNum + jiterationNum == target)
+                    if (iterationNum + jiterationNum == target) // if the values are equal to the target
                     {
-                        TwoSumArray[0] = i;
-                        TwoSumArray[1] = j;
+                        TwoSumArray[0] = i; // add index position i to array
+                        TwoSumArray[1] = j; // add index position j to array
                         //return TwoSumArray;
                         Console.WriteLine(string.Join(",", TwoSumArray));
                         Console.ReadLine();
